b600ea64f5ba7146fe064537fd5d65cd57c2f537
wahyu alfarisi <wahyualfarisi@gmail.com>
2018-08-10 21:36:21 +0700

    hello

--- application/views/admin/main.php
+++ application/views/admin/main.php
@@ -13,7 +13,7 @@
     <link rel="apple-touch-icon" sizes="76x76" href="../../app-assets/images/ico/apple-icon-76.png">
     <link rel="apple-touch-icon" sizes="120x120" href="../../app-assets/images/ico/apple-icon-120.png">
     <link rel="apple-touch-icon" sizes="152x152" href="../../app-assets/images/ico/apple-icon-152.png">
-    <link rel="shortcut icon" type="image/x-icon" href="../../app-assets/images/ico/favicon.ico">
+    <link rel="shortcut icon" type="image/x-icon" href="<?= base_url().'images/logo111.png' ?>">
     <link rel="shortcut icon" type="image/png" href="../../app-assets/images/ico/favicon-32.png">
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-touch-fullscreen" content="yes">
