08d94f45cbaa9696da9d088e3f333ec23b9d8066
havizIM <viz.ndinq@gmail.com>
2018-08-10 09:32:49 +0700

    Update Haviz

--- application/controllers/Json.php
+++ application/controllers/Json.php
@@ -7,12 +7,24 @@ class Json extends CI_Controller {
   {
     parent::__construct();
     $this->load->model('m_main');
-    $this->load->model('m_main');
+    $this->load->model('m_master');
   }
 
   function json_penduduk()
   {
     $data['penduduk'] = $this->m_main->select('t_penduduk', null)->result();
+    echo json_encode($data);
+  }
+
+  function json_staff()
+  {
+    $data['staff'] = $this->m_main->select('t_staff', null)->result();
+    echo json_encode($data);
+  }
+
+  function json_keterangan()
+  {
+    $data['keterangan'] = $this->m_master->show_keterangan(null, null)->result();
     echo json_encode($data);
   }
 
--- application/models/M_master.php
+++ application/models/M_master.php
@@ -20,9 +20,10 @@
 
     function show_keterangan($where = null, $between = null)
     {
-      $this->db->select('a.komentar, b.nama_peserta');
-      $this->db->from('t_komentar a');
-      $this->db->join('t_peserta b', 'b.id_peserta = a.id_peserta', 'left');
+      $this->db->select('a.*, b.*, c.username');
+      $this->db->from('t_keterangan a');
+      $this->db->join('t_penduduk b', 'b.id_penduduk = a.id_penduduk', 'left');
+      $this->db->join('t_user c', 'c.username = a.input_by', 'left');
 
       if($where != null)
       {
